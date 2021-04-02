d24fa8cb41acc0eb193c3c88854cacaf5fde51ca
pranavnimbagiri <67331631+pranavnimbagiri@users.noreply.github.com>
2021-04-02 18:26:15 +0530

    Add files via upload

---
+++ guessinggame.py
@@ -0,0 +1,13 @@
+intro=input("guess a number between 1-9")
+print(intro)
+
+
+
+eg=int(input("enter your guess:"))
+if (eg>3):
+    print("your guess is wrong:guess more than 3")
+elif(eg>5):
+    print("your guess is wrong:guess more than 5")
+else:
+    print("your guess is wrong:guess more than 7")
+
